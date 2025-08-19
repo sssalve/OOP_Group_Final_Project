e686c76b7b69788c984995ac0aef46414e15b5ba
CodyDavidsonSD <cody.davidson1@edu.sait.ca>
2025-08-18 20:56:38 -0600

    adds hourly and salary classes
    adds FullTime and Salaried classes inheriting from Employee class converts Employee to an abstract class, creates selection for which pay type to use when adding an employee

---
+++ Salaried.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Group_Final_Project
+{
+    public class Salaried : Employee
+    {
+        public decimal Salary { get; set; }
+
+        public override decimal GetPay(decimal weeks = 54)
+        {           
+            return (Salary / 54) * weeks;
+        }
+    }
+}
